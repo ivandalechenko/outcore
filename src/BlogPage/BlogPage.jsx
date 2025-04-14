950f652607df00bd31e4891b496bb404013fe457
logikmars <dima76673@gmail.com>
2025-04-14 22:21:57 +0300

    fix

--- src/BlogPage/BlogPage.jsx
+++ src/BlogPage/BlogPage.jsx
@@ -1,7 +1,28 @@
 import BlogItem from './BlogItem/BlogItem';
 import './BlogPage.scss';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 export default () => {
+
+    const useIsLandscape = () => {
+        const getIsLandscape = () => window.innerWidth > window.innerHeight;
+        const [isLandscape, setIsLandscape] = useState(getIsLandscape());
+      
+        useEffect(() => {
+          const handleResize = () => {
+            setIsLandscape(getIsLandscape());
+          };
+      
+          window.addEventListener('resize', handleResize);
+          window.addEventListener('orientationchange', handleResize);
+      
+          return () => {
+            window.removeEventListener('resize', handleResize);
+            window.removeEventListener('orientationchange', handleResize);
+          };
+        }, []);
+      
+        return isLandscape;
+      };
 
     const filters = [
         {name: 'All'},
@@ -61,6 +82,22 @@ export default () => {
             title: 'Typical SEO Mistakes: Why your website ranks low',
             description: 'SEO is a complex strategy, and no strategy comes without risks. When improving a website’s position, it’s...'
         },
+        {
+            src: '/blog/6.webp',
+            date: '02.02.2025',
+            time: 'Время чтения: 5 мин',
+            hashtag: '#Event Insights',
+            title: 'Typical SEO Mistakes: Why your website ranks low',
+            description: 'SEO is a complex strategy, and no strategy comes without risks. When improving a website’s position, it’s...'
+        },
+        {
+            src: '/blog/6.webp',
+            date: '02.02.2025',
+            time: 'Время чтения: 5 мин',
+            hashtag: '#Event Insights',
+            title: 'Typical SEO Mistakes: Why your website ranks low',
+            description: 'SEO is a complex strategy, and no strategy comes without risks. When improving a website’s position, it’s...'
+        },
     ]
 
     const [activeFilter, setActiveFilter] = useState('All');
@@ -68,6 +105,16 @@ export default () => {
     const handleFilterClick = (filterName) => {
         setActiveFilter(filterName);
     };
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const isLandscape = useIsLandscape();
+    const itemsPerPage = isLandscape ? 6 : 3;
+
+    const totalPages = Math.ceil(gallery.length / itemsPerPage);
+
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentGallery = gallery.slice(indexOfFirstItem, indexOfLastItem);
 
     return (
         <div className='BlogPage'>
@@ -86,10 +133,45 @@ export default () => {
                 </div>
             </div>
             <div className='BlogPage__gallery'>
-                    {gallery.map((el, index) => (
-                        <BlogItem src={el.src} date={el.date} time={el.time} hashtag={el.hashtag} title={el.title} description={el.description}/>
-                    ))}
+                {currentGallery.map((el, index) => (
+                    <BlogItem
+                    key={index}
+                    src={el.src}
+                    date={el.date}
+                    time={el.time}
+                    hashtag={el.hashtag}
+                    title={el.title}
+                    description={el.description}
+                    />
+                ))}
             </div>
+            <div className="BlogPage__pagination">
+                <button 
+                    onClick={() => setCurrentPage(prev => Math.max(prev - 1, 1))}
+                    disabled={currentPage === 1}
+                    className="BlogPage__pagination_arrow"
+                >
+                    <img src="/arrowGallery.svg" alt="" />
+                </button>
+
+                {[...Array(totalPages)].map((_, index) => (
+                    <button
+                    key={index}
+                    onClick={() => setCurrentPage(index + 1)}
+                    className={`BlogPage__pagination_item ${currentPage === index + 1 ? 'active' : ''}`}
+                    >
+                    {index + 1}
+                    </button>
+                ))}
+
+                <button 
+                    onClick={() => setCurrentPage(prev => Math.min(prev + 1, totalPages))}
+                    disabled={currentPage === totalPages}
+                    className="BlogPage__pagination_arrow"
+                >
+                    <img src="/arrowGallery.svg" alt="" className='BlogPage__pagination_arrow_img2'/>
+                </button>
+                </div>
         </div>
     )
 }
