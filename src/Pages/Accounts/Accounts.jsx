5784fb34422dcc92c2ee71414c7a8fb4c2e69239
logikmars <dima76673@gmail.com>
2025-04-15 14:10:17 +0300

    fix

--- src/Pages/Accounts/Accounts.jsx
+++ src/Pages/Accounts/Accounts.jsx
@@ -26,7 +26,7 @@ export default () => {
             spend: 507,
             age: 2024,
             profile: 'old pay',
-            country: 'AZ',
+            country: 'az',
             price: 220
         },
         {
@@ -35,7 +35,7 @@ export default () => {
             spend: 39100,
             age: 2020,
             profile: 'old pay',
-            country: 'SE',
+            country: 'se',
             price: 600
         },
         {
@@ -44,7 +44,7 @@ export default () => {
             spend: 72700,
             age: 2018,
             profile: 'Reset',
-            country: 'NO',
+            country: 'no',
             price: 900
         },
         {
@@ -53,7 +53,7 @@ export default () => {
             spend: 777777,
             age: 2026,
             profile: 'cuck old pay',
-            country: 'UA',
+            country: 'ua',
             price: 666
         },
         {
@@ -62,7 +62,7 @@ export default () => {
             spend: 507,
             age: 2024,
             profile: 'old pay',
-            country: 'AZ',
+            country: 'az',
             price: 220
         },
     ]
@@ -75,30 +75,34 @@ export default () => {
             <h2 className='Accounts__title'>
                 <span className='text__gradient'>Выберите</span> идеальный <br /> Google аккаунт для залива
             </h2>
-            <div className='Accounts__form'>
-                <div className='Accounts__form_links'>
-                    {links.map((el, index) => (
-                        <a href='#' className='Accounts__form_links_item' key={index}>{el.name}</a>
-                    ))}
-                </div>
-                <div className='Accounts__form_filter'>
-                    <Filter />
-                </div>
-                <div className='Accounts__form_form'>
-                    {names.map((el, index) => (
-                        <div className='Accounts__form_form_item'>{el.name}</div>
-                    ))}
-                    {items.map((el, index) => (
-                        <div className='Accounts__form_form_el'>
-                            <div className='Accounts__form_form_el_item'>{el.id}</div>
-                            <div className='Accounts__form_form_el_item'>{el.currency}</div>
-                            <div className='Accounts__form_form_el_item'>{el.spend}</div>
-                            <div className='Accounts__form_form_el_item'>{el.age}</div>
-                            <div className='Accounts__form_form_el_item'>{el.profile}</div>
-                            <div className='Accounts__form_form_el_item'>{el.country}</div>
-                            <div className='Accounts__form_form_el_item'>{el.price}</div>
-                        </div>
-                    ))}
+            <div className='Accounts__form_border'>
+                <div className='Accounts__form'>
+                    <div className='Accounts__form_links'>
+                        {links.map((el, index) => (
+                            <a href='#' className='Accounts__form_links_item' key={index}>{el.name}</a>
+                        ))}
+                    </div>
+                    <div className='Accounts__form_filter'>
+                        <Filter />
+                    </div>
+                    <div className='Accounts__form_form'>
+                        {names.map((el, index) => (
+                            <div className='Accounts__form_form_item'>{el.name}</div>
+                        ))}
+                        {items.map((el, index) => (
+                            <div className='Accounts__form_form_el'>
+                                <div className='Accounts__form_form_el_item'>{el.id}</div>
+                                <div className='Accounts__form_form_el_item'>{el.currency}</div>
+                                <div className='Accounts__form_form_el_item'>{el.spend}</div>
+                                <div className='Accounts__form_form_el_item'>{el.age}</div>
+                                <div className='Accounts__form_form_el_item'>{el.profile}</div>
+                                <div className='Accounts__form_form_el_item Accounts__form_form_el_item_country'>
+                                    <img src={`https://flagcdn.com/${el.country}.svg`} alt="" />
+                                    {el.country}</div>
+                                <div className='Accounts__form_form_el_item'>{el.price}</div>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
